5ac0a38f081b21811655ec166bcb62d39126340f
Victor <Victor@FujitsuT5010>
2017-04-26 12:16:18 +0200

    commit

---
+++ 1/src/uno.java
@@ -0,0 +1,16 @@
+
+public class uno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String texto="";
+		int punto=0, 
+		Scanner sc=new Scanner (System.in);
+		
+		System.out.print("Introduce un testo y termina lo con un punto.");
+		texto=sc.nextLine();
+		
+
+	}
+
+}
